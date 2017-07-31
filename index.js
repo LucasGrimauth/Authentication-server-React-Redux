4d8427043f0ae0ffe956edebdb0f52912dae0a29
Lucas Grimauth <lgevan@br.ibm.com>
2017-07-31 11:50:52 -0300

    Signin complete

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB Setup
 mongoose.connect('mongodb://localhost:auth/auth');
@@ -14,11 +15,14 @@ mongoose.connect('mongodb://localhost:auth/auth');
 
 // Register Middlewares
 app.use(morgan('combine')); //debugging
+// CORS protects users from connecting to other domains, subdomains or ports
+// so the server needs a way to the client when using another domain/subdomain/port
+app.use(cors()); // Handle 	CORS
 app.use(bodyParser.json({ type: '*/*'}));
 router(app);
 
 // Server Setup
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const server = http.createServer(app);
 server.listen(port);
 console.log('Server  listening on: ', port);
